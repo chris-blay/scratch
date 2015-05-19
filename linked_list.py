9842d6d227599e82907aaa91a00c70b14f24729e
Chris Blay <chris.b.blay@gmail.com>
2015-05-18 22:21:40 -0700

    Cleaned up linked_list.py a bit. Added some new test cases

--- linked_list.py
+++ linked_list.py
@@ -30,19 +30,15 @@ class LinkedList:
         current = self.head
         while current.next:
             current = current.next
-        current.next = _Node(value, current.next)
-        
+        current.next = _Node(value)
 
     def appendfront(self, value):
         '''append value to front.'''
-        current = self.head
-        while True:
-            current.next = _Node(value, current.next)
-            break
+        self.head.next = _Node(value, self.head.next)
 
     def clear(self):
         '''remove all values.'''
-        if self.head.next != None:
+        if self.head.next is not None:
             self.head.next = self.head.next.next
             clear(self)
 
@@ -51,11 +47,9 @@ class LinkedList:
         current = self.head
         count = 0
         while current.next:
-            if current.next.value == value:
+            current = current.next
+            if current.value == value:
                 count += 1
-                current = current.next
-            else:
-                current = current.next
         return count
 
     def index(self, value):
@@ -66,17 +60,16 @@ class LinkedList:
         current = self.head
         count = 0
         while current.next:
-            if current.next.value == value:
+            current = current.next
+            if current.value == value:
                 return count
-            else: 
-                current = current.next
-                count += 1
+            count += 1
         raise ValueError('Could not find %r in list' % value)
 
     def pop(self):
         '''remove and return last item.'''
         current = self.head
-        if current.next == None:
+        if current.next is None:
             return None
         while current.next.next:
             current = current.next
@@ -87,7 +80,7 @@ class LinkedList:
     def popfront(self):
         '''remove and return first item.'''
         current = self.head
-        if current.next == None:
+        if current.next is None:
             return None
         popped = current.next.value
         current.next = current.next.next
@@ -99,7 +92,7 @@ class LinkedList:
         Raises ValueError if the value is not present.
         '''
         current = self.head
-        if current.next == None:
+        if current.next is None:
             raise ValueError('List is empty, brah.')
         while current.next.next:
             if current.next.value == value:
@@ -109,17 +102,16 @@ class LinkedList:
                 current = current.next
             raise ValueError('%r not in here, son!' % value)
 
-    def getindex(self, index):
+    def __getitem__(self, index):
         current = self.head
         count = 0
         while count != index:
-            if current.next == None:
+            if current.next is None:
                 raise IndexError('List not that long.')
             else:
                 count += 1
                 current = current.next
         return current.next.value
-
 
 
 class LinkedListTestCase(unittest.TestCase):
@@ -148,16 +140,55 @@ class LinkedListTestCase(unittest.TestCase):
         self.assertEqual(0, self.list.index(42))
 
     def test_remove(self):
-        self.list.append(42)
-        self.list.remove(42)
+        for i in range(3):
+            self.list.append(i)
+        for i in reversed(range(3)):
+            self.list.remove(i)
 
     def test_count(self):
         self.list.append(42)
         self.assertEqual(1, self.list.count(42))
 
-    # TODO: Add more test cases as needed.
+    def test_clear_while_empty(self):
+        self.list.clear()
+        self.assertEqual(0, len(self.list))
+
+    def test_clear(self):
+        for i in range(3):
+            self.list.append(i)
+        self.list.clear()
+        self.assertEqual(0, len(self.list))
+
+    def test_getitem_while_empty(self):
+        self.assertRaises(IndexError, self.list[0])
+        self.assertRaises(IndexError, self.list[1])
+        self.assertRaises(IndexError, self.list[10])
+        self.assertRaises(IndexError, self.list[-1])
+
+    def test_getitem(self):
+        for i in range(3):
+            self.list.append(i)
+            self.assertEqual(i, self.list[i])
+        self.assertEqual(3, self.list[-1])
+
+    def test_pop_while_empty(self):
+        self.assertEqual(None, self.list.pop())
+
+    def test_pop(self):
+        for i in range(3):
+            self.list.append(i)
+        for i in reversed(range(3)):
+            self.assertEqual(i, self.list.pop())
+
+    def test_popfront_while_empty(self):
+        self.assertEqual(None, self.list.popfront())
+
+    def test_popfront(self):
+        for i in range(3):
+            self.list.append(i)
+        for i in range(3):
+            self.assertEqual(i, self.list.popfront())
 
 
 if __name__ == '__main__':
     unittest.main()
-
